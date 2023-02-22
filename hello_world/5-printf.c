1bb14fc6ffb6b64c2f12f76c1a8031468da9624d
azizios011 <hdriaziz699@gmail.com>
2023-02-22 01:42:50 -0800

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,6 @@
 #!/bin/bash
-printf ("Programming is like building a multilingual puzzle") return 0
+#include <stdio.h>
+int main(void){
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return 0;
+}
