bb0351201103903aa48db6c8c95e1ac37a9e6a18
azizios011 <hdriaziz699@gmail.com>
2023-02-22 01:56:30 -0800

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-int puts(Programming is like building a multilingual puzzle* value+0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+          puts("Programming is like building a multilingual puzzle");
+	 return 0;
+}
