ab6152e6738ba496f602642e42627932f788ad15
azizios011 <hdriaziz699@gmail.com>
2023-02-28 02:55:52 -0800

    Does not appear to be a unified-diff format patch

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	digit = n % 10;
+	/* your code goes there */
+
+	if (n > 5)
+	{
+		printf("Last digit of %d is %i and is greater than 5\n", n, digit);
+	}
+
+	else if (n < 6)
+	{
+		printf("Last digit of %d is %i and is less than 6 and not 0\n", n, digit);
+	}
+
+	else
+	{
+		printf("Last digit of %d is %i and is 0\n", n, digit);
+	}
+	return (0);
+}
