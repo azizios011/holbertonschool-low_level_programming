6facf72548428a939f66aa5fe1a0ead3a9e581a5
azizios011 <hdriaziz699@gmail.com>
2023-03-01 04:12:49 -0800

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	i = 1;
+	y = 48;
+
+	while (i < 11)
+	{
+		putchar (y);
+		i++;
+		y++;
+	}
+		putchar('\n');
+		return (0);
+}
+
