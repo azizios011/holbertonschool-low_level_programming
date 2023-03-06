24c4e672f091169088cce628b2d19fca02481e1f
azizios011 <hdriaziz699@gmail.com>
2023-03-06 03:00:20 -0800

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int count;
 	char letter;
 
-	for (count = 0 ; count <= 10 ; count++)
+	for (count = 0 ; count <= 9 ; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
