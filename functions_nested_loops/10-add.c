c7c3ae378388a6e5ac7e566950a39da1e628407d
azizios011 <hdriaziz699@gmail.com>
2023-03-08 04:34:12 -0800

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - check the code
+ *
+ * @n1 : number1.
+ * @n2 :number2.
+ *
+ * Return: Always 0.
+ */
+int add(int n1, int n2)
+{
+	int r;
+
+	r = n1 + n2;
+	_putchar(r);
+	_putchar('\n');
+	return (r);
+}
