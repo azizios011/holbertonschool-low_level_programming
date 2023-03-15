e152f9ae82d47a3f11c7b062aba8984790cbc977
azizios011 <hdriaziz699@gmail.com>
2023-03-15 01:39:34 -0700

    hahahaha

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef HEADER
 #define HEADER
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-int _putchar(char c);
+void _puts(char *str);
 #endif
