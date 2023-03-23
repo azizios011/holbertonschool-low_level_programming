0cf8e3e55026ec639e627956219cb3434221d0ba
azizios011 <hdriaziz699@gmail.com>
2023-03-23 03:53:31 -0700

    strcat

---
+++ more_pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat - check the code
+ *
+ * @src : src.
+ * @dest: dest.
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
