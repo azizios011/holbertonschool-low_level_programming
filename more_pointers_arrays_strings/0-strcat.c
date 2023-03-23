08d929d073cee48cbc5338e578e2c7fb9e549cb6
azizios011 <hdriaziz699@gmail.com>
2023-03-23 04:38:03 -0700

    strcat

--- more_pointers_arrays_strings/0-strcat.c
+++ more_pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int i = 0, j;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	for (j = 0; src[j] != 0; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
+
