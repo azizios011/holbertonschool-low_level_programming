88976b1d41c963f8b498fd1230fa89306eefc5ba
azizios011 <hdriaziz699@gmail.com>
2023-03-25 09:22:41 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * cap_string - check the code
+ * @str : string.
+ * Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ')
+		{
+			str[i] = toupper(str[i]);
+		}
+	}
+	return (str);
+}
