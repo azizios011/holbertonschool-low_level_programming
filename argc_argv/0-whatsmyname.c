29b9e92da88f14a3b10acb96b8f555a56ceb7d35
azizios011 <hdriaziz699@gmail.com>
2023-03-29 02:07:09 -0700

    It aint what they call you, its what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc : argument count
+ * @argv : argument vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void) argc;
+
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
