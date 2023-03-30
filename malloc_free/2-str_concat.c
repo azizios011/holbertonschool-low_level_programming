9e19044cfa2038d86f14822faebad748c773e1db
azizios011 <hdriaziz699@gmail.com>
2023-03-30 05:42:11 -0700

    He who is not courageous enough to take risks will accomplish nothing in

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,8 +10,9 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
+	int len1 = 0;
+	int len2 = 0;
+	int i;
 	char *r;
 
 	if (s1 == NULL)
@@ -22,13 +23,28 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	r = malloc(len1 + len2 + 1);
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s1[len2] != '\0')
+	{
+		len2++;
+	}
 
+	r = malloc((len1 + len2 + 1) * sizeof(char));
 	if (r == NULL)
 	{
 		return (NULL);
 	}
-	memcpy(r, s1, len1);
-	memcpy(r + len1, s2, len2 + 1);
+	for (i = 0; i < len1; i++)
+	{
+		r[i] = s1[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		r[len2 + 1] = s2[i];
+	}
+	r[len1 + len2] = '\0';
 	return (r);
 }
