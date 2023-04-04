37c90c33ddc42f75bc7b40c4fe206b64841e4f85
azizios011 <hdriaziz699@gmail.com>
2023-04-04 00:16:39 -0700

    Trust no one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - check the code
+ * @b : unsigned parameter
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		fprintf(stderr, "%u\n", b);
+		abort();
+	}
+	return (p);
+}
