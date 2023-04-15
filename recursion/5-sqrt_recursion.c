da3ff5a923f34c0882770328f8184bcb93a84236
azizios011 <azizhidri001@gmail.com>
2023-04-15 04:03:44 -1100

    Your subconscious is looking for the dreamer

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,24 +11,38 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
+
+	return (sqrt_helper(n, 0, n));
+}
+
+/**
+ * sqrt_helper - check the code
+ * @n: the number whose square root is to be found
+ * @start:  the lower bound of the range of possible square roots
+ * @end: the upper bound of the range of possible square roots
+ * Return: value.
+*/
+int sqrt_helper(int n, int start, int end)
+{
+	int mid;
+
+	if (start > end)
 	{
-		return (n);
+		return (-1);
+	}
+
+	mid = (start + end) / 2;
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid < n)
+	{
+		return (sqrt_helper(n, mid + 1, end));
 	}
 	else
 	{
-		int i;
-
-		for (i = 1; i * i <= n; i++)
-		{}
-		i--;
-		if (i * i == n)
-		{
-			return (i);
-		}
-		else
-		{
-			return (-1);
-		}
+		return (sqrt_helper(n, start, mid - 1));
 	}
 }
