5c14634baad4af07be0d0a661fdf5e5ca0894e47
azizios011 <azizhidri001@gmail.com>
2023-04-17 00:53:10 -1100

    Sum list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - check the code
+ * @head:  Pointer to the head of the linked list.
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
