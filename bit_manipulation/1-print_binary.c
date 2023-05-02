036fda636d38c2d526163510a94f092ce54f71cb
azizios011 <azizhidri001@gmail.com>
2023-05-02 09:06:13 -1100

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,33 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to print in binary
- * Return: Always 0.
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1;
+	int flag = 0;
 
-	/* Check if n is zero */
+	mask <<= (sizeof(unsigned long int) * 8 - 1);
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 
-	/* Find the highest bit that is set */
-	while ((mask << 1) <= n)
-		mask <<= 1;
-
-	/* Print the binary digits */
-	while (mask != 0)
+	while (mask)
 	{
 		if (n & mask)
+		{
 			_putchar('1');
-		else
+			flag = 1;
+		}
+		else if (flag)
 			_putchar('0');
+
 		mask >>= 1;
 	}
+
 }
